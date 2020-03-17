0210db811a8cbf21962e7f8943c81b972680abd5
Dana <dana.ottaviani@gmail.com>
2020-03-17 17:21:34 -0400

    finish tutorial

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const ApolloServer = require('apollo-server').ApolloServer
+const ApolloServerLambda = require('apollo-server-lambda').ApolloServer
+const { gql } = require('apollo-server-lambda')
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => "Hello World!"
+    }
+};
+
+function createLambdaServer () {
+    return new ApolloServerLambda({
+        typeDefs,
+        resolvers,
+        introspection: true,
+        playground: true,
+    });
+}
+
+function createLocalServer () {
+    return new ApolloServer({
+        typeDefs,
+        resolvers,
+        introspection: true,
+        playground: true
+    })
+}
+
+module.exports = { createLambdaServer, createLocalServer }
